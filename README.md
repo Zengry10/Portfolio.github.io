28bcac949df568de488d98480185cb7de0a0e995
Zengry10 <remi.lemoust-de-lafosse@epitech.digital>
2022-03-21 16:09:00 +0100

    Final Repository

--- README.md
+++
@@ -1,1 +0,0 @@
-# Zengry10.github.io
--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developpeur", "JavaScript", "HTML/CSS", "PHP", "Python"],
+        strings: ["Developpeur", "Student", "Developpeur", "French"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developpeur", "Javascript", "HTML/CSS", "PHP", "Python"],
+        strings: ["Developpeur", "Student", "Developpeur", "French"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
