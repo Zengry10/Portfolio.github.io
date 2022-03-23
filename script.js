5b25cddf14f9724cab8c2df298c96a1655bff340
Zengry10 <remi.lemoust-de-lafosse@epitech.digital>
2022-03-23 16:42:18 +0100

    final change

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developpeur", "Student", "Developpeur", "French"],
+        strings: ["Developer", "Student", "French"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developpeur", "Student", "Developpeur", "French"],
+        strings: ["Developer", "Student", "French"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
